83566d459dac1a8ec066392b840a8751358629b3
Alex_kabs <kabudurajonathan@gmail.com>
2024-10-28 08:53:53 +0200

    Initial Version

---
+++ index.js
@@ -0,0 +1,10 @@
+/*console.log("I am Batman");
+console.log("I love pizza");
+
+window.alert(`Valar Moghulis`);
+window.alert(`Valar Doharis`);
+*/
+
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I Am batman ";
